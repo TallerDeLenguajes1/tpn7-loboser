f4eac63fc8dc6c29437612ec313d1bcda94c337c
loboser <loboser.sl@gmail.com>
2020-05-28 12:08:08 -0300

    App

---
+++ App/App/Calculadora.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App
+{
+    class Calculadora
+    {
+        private float numero1;
+        private float numero2;
+
+        public float Numero1 { get => numero1; set => numero1 = value; }
+        public float Numero2 { get => numero2; set => numero2 = value; }
+
+        public float Suma()
+        {
+            return Numero1 + Numero2;
+        }
+
+        public float Resta()
+        {
+            return Numero1 - Numero2;
+        }
+
+        public float Multiplicacion()
+        {
+            return Numero1 * Numero2;
+        }
+
+        public float Division()
+        {
+            return Numero1 / Numero2;
+        }
+    }
+}
